3d1d09bc3fe0edc6b0dd02a604a61ae003483457
Yandri-99 <lumiquinga724@gmail.com>
2026-01-06 21:32:47 -0500

    2025-01-06

--- modulo_3/ReactJS/my-react-app/src/App.tsx
+++ modulo_3/ReactJS/my-react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import HelloWorld from './components/HelloWorld';
+/*import HelloWorld from './components/HelloWorld';
 import WelcomeUser from './components/WelcomeUser';
 import ButtonCounter from './components/ButtonCounter';
 import TextInput from './components/TextInput';
@@ -37,6 +37,50 @@ function App() {
       </tr>
     </>
   );
+}*/
+
+import Greeting from './props/Greeting';
+import UserCard from './props/UserCard';
+import ButtonOne from './props/ButtonOne';
+import IncrementButton from './props/IncrementButton';
+import ParentInputNotifier from './props/ParentInputNotifier';
+import HoverFont from './hood/HoverFont';
+import AccessWithLimit from './hood/AccessWithLimit';
+import DocumentTitleChanger from './hood/DocumentTitleChanger';
+import PostLikes from './hood/PostLikes';
+import CheckboxSummary from './hood/CheckboxSummary';
+import LanguageSwitcher from './hood/LanguageSwitcher';
+import MultiSwitch from './hood/MultiSwitch';
+import AutoCounter from './hood/AutoCounter';
+import DynamicColor from './hood/DynamicColor';
+import ClickHistory from './hood/ClickHistory';
+
+
+
+function App() {
+  const userData = {name: "Juan",age: 25};
+  
+  return (
+    <>
+      <Greeting name="Francisco" lastname="Perez" />
+      <Greeting name="Ana" lastname="Garcia" />
+      <UserCard user={userData} />
+      <ButtonOne onClick={() => console.log("Botón clickeado!")} />
+      <IncrementButton onIncrement={() => console.log("Botón clickeado!")} />
+      <ParentInputNotifier />
+      <HoverFont />
+      <AccessWithLimit />
+      <DocumentTitleChanger />
+      <PostLikes />
+      <CheckboxSummary />
+      <LanguageSwitcher />
+      <MultiSwitch />
+      <AutoCounter />
+      <DynamicColor />
+      <ClickHistory />
+    </>
+  );
 }
+
 
 export default App;
