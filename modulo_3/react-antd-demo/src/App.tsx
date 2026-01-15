f37477e623b07dc513278f35402638d9b095e203
Yandri-99 <lumiquinga724@gmail.com>
2026-01-14 21:57:45 -0500

    14-1-2026

---
+++ modulo_3/react-antd-demo/src/App.tsx
@@ -0,0 +1,19 @@
+import LabAntButtons from "./lab/LabAntButtons";
+import LabAntAlert from "./lab/LabAntAlert";
+import LabAntCard from "./lab/LabAntCard";
+import LabAntForm from "./lab/LabAntForm";
+import LabAntTable from "./lab/LabAntTable";
+
+function App() {
+  return (
+    <>
+      <LabAntButtons />
+      <LabAntAlert />
+      <LabAntCard />
+      <LabAntForm />
+      <LabAntTable />
+    </>
+  )
+}
+
+export default App
